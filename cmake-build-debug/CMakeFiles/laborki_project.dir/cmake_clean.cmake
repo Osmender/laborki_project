818ca2c526591e9ce0e26c92561e394d7e54eb55
osmender <linard.mateusz@gmail.com>
2022-01-11 20:43:33 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/laborki_project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laborki_project.dir/main.cpp.obj"
+  "laborki_project.exe"
+  "laborki_project.exe.manifest"
+  "laborki_project.pdb"
+  "liblaborki_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laborki_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
